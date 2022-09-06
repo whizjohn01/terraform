b1e0f6acda5909e1330748acefb88657450eb51f
whizjohn01 <jotopea@gmail.com>
2022-09-06 05:41:18 +0100

    Remove Previous Commits

---
+++ manifest1.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+  region = "us-east-1"
+
+}
+
+resource "aws_instance" "manifest1" {
+  ami           = "ami-0729e439b6769d6ab"
+  instance_type = "t2.micro"
+
+tags = {
+
+    # First instance
+    Name = "manifest1"
+}
+
+
+
+resource "aws_instance" "manifest2" {
+
+  count = 20 #Create 20 identical instances
+
+  #All 20 instances will have the same ami and instance_type
+  ami           = "ami-0729e439b6769d6ab"
+  instance_type = "t2.micro"
+p
+  tags = {
+
+    # The count.index allows you to launch a resource 
+    # starting with the distinct index number 0 and corresponding to this instance.
+    Name = "manifest2-${count.index + 1}"
+}
+}
+}
